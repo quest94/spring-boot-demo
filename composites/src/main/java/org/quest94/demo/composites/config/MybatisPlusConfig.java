50c71e8f2f7fdbded47e4b343dfc51ee90e01d2c
quest94 <quest94@163.com>
2024-01-18 13:50:25 +0800

    MybatisPlus 配置更正

--- composites/src/main/java/org/quest94/demo/composites/config/MybatisPlusConfig.java
+++ composites/src/main/java/org/quest94/demo/composites/config/MybatisPlusConfig.java
@@ -1,10 +1,28 @@
 package org.quest94.demo.composites.config;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
+import com.baomidou.mybatisplus.core.injector.AbstractMethod;
+import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.extension.MybatisMapWrapperFactory;
+import com.baomidou.mybatisplus.extension.injector.methods.AlwaysUpdateSomeColumnById;
+import com.baomidou.mybatisplus.extension.injector.methods.InsertBatchSomeColumn;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.type.JdbcType;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.List;
 
 /**
  * <P>ClassName: MybatisPlusConfig
@@ -17,6 +35,61 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @MapperScan("org.quest94.demo.composites.mapper.*")
 public class MybatisPlusConfig {
+    @Bean("mybatisSqlSession")
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource, GlobalConfig globalConfig) throws Exception {
+        MybatisConfiguration configuration = new MybatisConfiguration();
+        configuration.setJdbcTypeForNull(JdbcType.NULL);
+        /* 驼峰转下划线 */
+        configuration.setMapUnderscoreToCamelCase(true);
+        /* map 下划线转驼峰 */
+        configuration.setObjectWrapperFactory(new MybatisMapWrapperFactory());
+
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+
+        /* 自动填充插件 */
+//        globalConfig.setMetaObjectHandler(new MysqlMetaObjectHandler());
+
+        MybatisSqlSessionFactoryBean sqlSessionFactory = new MybatisSqlSessionFactoryBean();
+        /* 数据源 */
+        sqlSessionFactory.setDataSource(dataSource);
+        /* xml扫描 */
+        sqlSessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver()
+                .getResources("classpath*:mapper/*/*.xml"));
+        /* 启用别名，这样就无需写全路径类名了，具体可自行查阅资料 */
+        sqlSessionFactory.setTypeAliasesPackage("org.quest94.demo.composites.entity");
+        /* 枚举扫描 */
+        sqlSessionFactory.setTypeEnumsPackage("org.quest94.demo.composites.entity.*");
+        /* 扫描 typeHandler */
+//        sqlSessionFactory.setTypeHandlersPackage("com.baomidou.mybatisplus.samples.mysql.type");
+        sqlSessionFactory.setPlugins(mybatisPlusInterceptor);
+        sqlSessionFactory.setConfiguration(configuration);
+        sqlSessionFactory.setGlobalConfig(globalConfig);
+        return sqlSessionFactory.getObject();
+    }
+
+    @Bean
+    public GlobalConfig globalConfig() {
+        GlobalConfig conf = new GlobalConfig();
+        conf.setDbConfig(new GlobalConfig.DbConfig().setColumnFormat("`%s`"));
+        DefaultSqlInjector logicSqlInjector = new DefaultSqlInjector() {
+            /**
+             * 注入自定义全局方法
+             */
+            @Override
+            public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
+                List<AbstractMethod> methodList = super.getMethodList(mapperClass, tableInfo);
+                // 不要逻辑删除字段, 不要乐观锁字段, 不要填充策略是 UPDATE 的字段
+                methodList.add(new InsertBatchSomeColumn(t -> !t.isLogicDelete() && !t.isVersion() && t.getFieldFill() != FieldFill.UPDATE));
+                // 不要填充策略是 INSERT 的字段, 不要字段名是 column4 的字段
+                methodList.add(new AlwaysUpdateSomeColumnById(t -> t.getFieldFill() != FieldFill.INSERT && !t.getProperty().equals("column4")));
+                return methodList;
+            }
+        };
+        conf.setSqlInjector(logicSqlInjector);
+        return conf;
+    }
 
     /**
      * 分页插件
